e84d0d6a0ba77e068ee9e3cb8f65ff78cd2de176
tmaria <tarsi.maria@gmail.com>
2023-03-27 23:12:59 +0200

    Init

---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+export default function About({ posts }: any) {
+    return <div>About {JSON.stringify(posts)}</div>
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+    const {NODE_ENV, DEV_HOST, PROD_HOST} = process.env;
+    const host = NODE_ENV === 'development' ? DEV_HOST : PROD_HOST;
+    // Call an external API endpoint to get posts
+    const res = await fetch(host+'/test.json')
+    const posts = await res.json()
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts
+        },
+    }
+}
