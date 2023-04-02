856b4ef0d46f507a4589f1a405704f5be4a5aecd
mariatj <tarsi.maria1@rigd-loxia.nl>
2023-04-02 19:33:56 +0200

    Playing around with a design

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,32 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import navStyles from '../styles/Nav.module.css';
 
-import Link from 'next/link'
-
+let navElement: HTMLElement | null;
 
 export default function Nav() {
-    return(
-    <nav className={navStyles.nav}>
-        <ul>
-            <li>
-                <Link href='/'>Home</Link>
-            </li>
-            <li>
-                <Link href='/about'>About</Link>
-            </li>
-        </ul>
-    </nav>)
+
+    const [_hover, setHover] = useState(false);
+
+    const handleScroll = () => {
+        navElement = navElement ? navElement : document.getElementById('nav');
+        setHover(!!(window.scrollY > 0 && navElement));
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    });
+
+    return (
+        <nav id={ 'nav' } className={ `${ navStyles.nav } ${ _hover ? navStyles.hover : '' }` }>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+            </ul>
+        </nav>);
 }
