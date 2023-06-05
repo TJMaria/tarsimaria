ec4c7002517d26cd8b6ff7145c27c5d32fb5411b
tmaria <tarsi.maria@gmail.com>
2023-06-05 17:25:24 +0200

    Add gtag

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import LoadTitle from '../components/LoadTitle';
 import Projects from '../components/Projects';
 import pic from '../public/tmit.webp';
 import layoutStyles from '../styles/Layout.module.scss';
+import Script from 'next/script';
 
 const { container } = layoutStyles;
 const NAVBAR_HEIGHT = 64;
@@ -15,26 +16,41 @@ function Home({ isMobileView }: any) {
     useEffect(handleInitScrollBar(isMobileView, isLoaded), [isLoaded]);
 
     return (
-        <div className={ container }>
+        <div className={container}>
             <div id="scroll-progress"></div>
-            <LoadTitle setIsLoaded={ setIsLoaded }></LoadTitle>
+            <LoadTitle setIsLoaded={setIsLoaded}></LoadTitle>
 
             <Head>
                 <title>Tarsi Maria | TMiT </title>
                 <meta name="description"
-                      content="Tarsi Maria's development portfolio. Tarsi Maria is a full stack developer and owner of TMiT."/>
+                    content="Tarsi Maria's development portfolio. Tarsi Maria is a full stack developer and owner of TMiT." />
                 <meta name="og:description"
-                      content="Tarsi Maria's development portfolio. Tarsi Maria is a full stack developer and owner of TMiT."/>
-                <meta name="image" content={ pic.src }/>
-                <meta name="og:image" content={ pic.src }/>
-                {/* Prevent browser from remembering last scroll position */ }
-                <script dangerouslySetInnerHTML={ { __html: `history.scrollRestoration = "manual"` } }/>
+                    content="Tarsi Maria's development portfolio. Tarsi Maria is a full stack developer and owner of TMiT." />
+                <meta name="image" content={pic.src} />
+                <meta name="og:image" content={pic.src} />
+                {/* Prevent browser from remembering last scroll position */}
+                <script dangerouslySetInnerHTML={{ __html: `history.scrollRestoration = "manual"` }} />
+        
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-5N8PSCCVXZ"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-5N8PSCCVXZ');
+                    `}
+                </Script>Í
             </Head>
 
             <div>
-                <Intro isLoaded={ isLoaded }></Intro>
-                <Projects/>
-                <Contact/>
+                <Intro isLoaded={isLoaded}></Intro>
+                <Projects />
+                <Contact />
             </div>
         </div>
     );
@@ -42,7 +58,7 @@ function Home({ isMobileView }: any) {
 
 Home.getInitialProps = (ctx: any) => {
     const isMobileView = !!(ctx.req ? ctx.req.headers['user-agent'] : navigator.userAgent)
-    .match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
+        .match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
 
     return { isMobileView };
 };
