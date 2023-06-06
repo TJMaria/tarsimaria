3103de0e58fd6ab95ab5efc2c9ad722aceb19e78
tmaria <tarsi.maria@gmail.com>
2023-06-06 13:41:26 +0200

    refactor Gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,23 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 import Script from 'next/script';
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-5N8PSCCVXZ" />
-      <Script id='google-analytics' strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-5N8PSCCVXZ');
-          `,
-        }}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
